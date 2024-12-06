6e839749042548331536e643daf625948410e946
tekvishal <vishal.adheli@tekditechnologies.com>
2024-12-06 18:15:27 +0530

    added xml file

---
+++ webservices/src/Controller/INTERNAL_NAME_list.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @version    CVS: 1.0.0
+ * @package    Com_Employee
+ * @author     Vaibhav Shinde <vaibhav87shinde@gmail.com>
+ * @copyright  2024 Vaibhav Shinde
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+namespace Empvaibhav\Component\Employee\Api\Controller;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\Controller\ApiController;
+
+/**
+ * The XXX_UCFIRST_INTERNAL_NAME_FORCE_LIST_XXX controller
+ *
+ * @since  1.0.0
+ */
+class XXX_UCFIRST_INTERNAL_NAME_FORCE_LIST_XXXController extends ApiController 
+{
+	/**
+	 * The content type of the item.
+	 *
+	 * @var    string
+	 * @since  1.0.0
+	 */
+	protected $contentType = 'XXX_INTERNAL_NAME_FORCE_LIST_XXX';
+
+	/**
+	 * The default view for the display method.
+	 *
+	 * @var    string
+	 * @since  1.0.0
+	 */
+	protected $default_view = 'XXX_INTERNAL_NAME_FORCE_LIST_XXX';
+}
